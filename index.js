dd65d78505d7f5e265e64ddcb3757d328266886c
Jerinkc <jerinkc22@gmail.com>
2023-08-12 20:37:21 +0530

    Files

---
+++ index.js
@@ -0,0 +1,6 @@
+function expandAboutMe(){
+  const button = document.getElementById("action-center-expand-about-me")
+  const aboutMeSection = document.getElementById("more-about-me")
+  button.style.display = "none";
+  aboutMeSection.style.display = "unset";
+}
